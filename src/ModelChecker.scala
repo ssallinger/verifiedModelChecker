921ba6797d89882bc72ac66a252c4b0f82312395
Sarah Sallinger <sallingersa@stud.sbg.ac.at>
2016-08-12 14:20:30 +0200

    Refactor

--- src/ModelChecker.scala
+++ src/ModelChecker.scala
@@ -5,29 +5,27 @@ import sharedOBDDs._
 import CTL._
 
 object ModelChecker {
-  def root(b: (BDD, BigInt)) = b._2
-  def bdd(b: (BDD, BigInt)) = b._1
 
-  def sat(states: (BDD, BigInt), transitions: (BDD, BigInt), f: Formula): (BDD, BigInt) = {
+   def sat(states: RootedBDD, transitions: RootedBDD, f: Formula): RootedBDD = {
     f match {
       case True    => states
-      case False => (bdd(states), 0)
+      case False => RootedBDD(states.b, 0)
       case Prop(atom) => {
-        build(bdd(states), Variable(atom.i))
+        build(states.b, Variable(atom.i))
       }
       case Not(f1) => {
-        val b = sat(states, transitions, f1)
-        minus(bdd(b), root(states), root(b))
+        val bdd = sat(states, transitions, f1)
+        minus(bdd.b, states.root, bdd.root)
       }
       case And(f1, f2) => {
         val b1 = sat(states, transitions, f1)
-        val b2 = sat((bdd(b1), root(states)), transitions, f2)
-        intersect(bdd(b2), root(b1), root(b2))
+        val b2 = sat(RootedBDD(b1.b, states.root), transitions, f2)
+        intersect(b2.b, b1.root, b2.root)
       }
       case Or(f1, f2) => {
         val b1 = sat(states, transitions, f1)
-        val b2 = sat((bdd(b1), root(states)), transitions, f2)
-        union(bdd(b2), root(b1), root(b2))
+        val b2 = sat(RootedBDD(b1.b, states.root), transitions, f2)
+        union(b2.b, b1.root, b2.root)
       }
       case Implies(f1, f2) => sat(states, transitions, Or(f1, Not(f2)))
       //case AX(f1)          => sat(states, transitions, Not(EX(Not(f1))))
@@ -43,9 +41,9 @@ object ModelChecker {
   
   //EG, EU, EX form minimal adequate subset of temporal connectives
   //TODO implement
-  def satex(states: (BDD, BigInt), transitions: (BDD, BigInt), f: Formula): (BDD, BigInt) = states
-  def sateu(states: (BDD, BigInt), transitions: (BDD, BigInt), f1: Formula, f2: Formula): (BDD, BigInt) = states //see book
-  def sateg(states: (BDD, BigInt), transitions: (BDD, BigInt), f: Formula): (BDD, BigInt) = states //see Isabelle tutorial
+  def satex(states: RootedBDD, transitions: RootedBDD, f: Formula): RootedBDD = states
+  def sateu(states: RootedBDD, transitions: RootedBDD, f1: Formula, f2: Formula): RootedBDD = states //see book
+  def sateg(states: RootedBDD, transitions: RootedBDD, f: Formula): RootedBDD = states //see Isabelle tutorial
 
 }
 
--- src/sharedOBDDs.scala
+++ src/sharedOBDDs.scala
@@ -4,7 +4,9 @@ import leon.annotation._
 
 object sharedOBDDs {
 
-  case class BDD(nodes: List[BigInt], T: Map[BigInt, (BigInt, BigInt, BigInt)], H: Map[(BigInt, BigInt, BigInt), BigInt], size: BigInt)
+  case class Node(variable: BigInt, low: BigInt, high: BigInt)
+  case class BDD(nodes: List[BigInt], T: Map[BigInt, Node], H: Map[Node, BigInt], size: BigInt)
+  case class RootedBDD(b: BDD, root: BigInt)
 
   abstract class Expression
   case object Top extends Expression
@@ -14,18 +16,20 @@ object sharedOBDDs {
   case class Conjunction(e1: Expression, e2: Expression) extends Expression
   case class Disjunction(e1: Expression, e2: Expression) extends Expression
  
-  def initT(numberOfVars: BigInt): Map[BigInt, (BigInt, BigInt, BigInt)] = {
-    Map(BigInt(0) -> (numberOfVars + 1, BigInt(-1), BigInt(-1)), BigInt(1) -> (numberOfVars + 1, BigInt(-1), BigInt(-1)))
+  //create new Table containing the terminal nodes (ID 0 and 1)
+  def initT(numberOfVars: BigInt): Map[BigInt, Node] = {
+    Map(BigInt(0) -> Node(numberOfVars + 1, BigInt(-1), BigInt(-1)), BigInt(1) -> Node(numberOfVars + 1, BigInt(-1), BigInt(-1)))
   }
 
-  def initH(): Map[(BigInt, BigInt, BigInt), BigInt] = Map()
+  def initH(): Map[Node, BigInt] = Map()
 
-  
+  //Map m is wellformed if it contains all elements of List l
   def wellFormed[X,Y](l: List[X], m: Map[X,Y]): Boolean = l match {
     case Nil() => true
     case Cons(x, xs) => m.contains(x) && wellFormed(xs,m)
   }
   
+  //updating a map preserves property of being wellformed
   @induct
   def wellFormedUpdate[X,Y](l: List[X], m: Map[X,Y], x: X, y: Y): Boolean = {
     require(wellFormed(l,m))
@@ -33,48 +37,49 @@ object sharedOBDDs {
     wellFormed(x :: l, m.updated(x,y))
   } holds 
   
-  //smarter allocation scheme for ids needed?
-  def add(b: BDD, i: BigInt, l: BigInt, h: BigInt): BDD = {
-    BDD(b.size :: b.nodes, b.T.updated(b.size, (i, l, h)), b.H.updated((i, l, h), b.size), b.size + 1)
+  def add(b: BDD, n: Node): BDD = {
+    //smarter allocation scheme for ids needed?
+    BDD(b.size :: b.nodes, b.T.updated(b.size, n), b.H.updated(n, b.size), b.size + 1)
   }
   
-  def wellFormedAdd(b: BDD, i: BigInt, l: BigInt, h: BigInt): Boolean = {
+  //add preserves wellformedness
+  def wellFormedAdd(b: BDD, n: Node): Boolean = {
     require(wellFormed(b.nodes,b.T))
     
-    val res = add(b,i,l,h)
+    val res = add(b, n)
     b.nodes.content.subsetOf(res.nodes.content) &&
-    wellFormedUpdate(b.nodes, b.T, b.size, (i,l,h)) &&
+    wellFormedUpdate(b.nodes, b.T, b.size, n) &&
     wellFormed(res.nodes,res.T)
   } holds
-
-  def variable(b: BDD, node: BigInt): BigInt = {
-    require(b.T.contains(node))
-    b.T(node)._1
+  
+  def getNode(b: BDD, id: BigInt): Node = {
+    require(b.T.contains(id))
+    b.T(id)
   }
-  def low(b: BDD, node: BigInt): BigInt = {
-    require(b.T.contains(node))
-    b.T(node)._2
-  }
-  def high(b: BDD, node: BigInt): BigInt = {
-    require(b.T.contains(node))
-    b.T(node)._3
-  }
-
-  def testAndInsert(b: BDD, i: BigInt, l: BigInt, h: BigInt): (BDD, BigInt) = {
-    if (l == h)
-      (b, l)
-    else if (b.H.contains((i, l, h)))
-      (b, b.H((i, l, h)))
+  
+  //look for node and create it if it does not exist
+  def testAndInsert(b: BDD, n: Node): RootedBDD = {
+    if (n.low == n.high)
+      RootedBDD(b, n.low)
+    else if (b.H.contains(n))
+      RootedBDD(b, b.H(n))
     else {
-      val bNew = add(b, i, l, h)
-      (bNew, b.size) //adapt if allocation scheme for IDs changes!
+      RootedBDD(add(b, n), b.size) //adapt if allocation scheme for IDs changes!
     }
   }
   
-//   def wellFormedTestAndInsert(b: BDD, i: BigInt, l: BigInt, h: BigInt)
+//   def wellFormedTestAndInsert(b: BDD, i: BigInt, l: BigInt, h: BigInt) //TODO
   
-  //proof: see leon example 
+  def isConstant(e: Expression) : Boolean = e match {
+    case Top => true
+    case Bottom => true
+    case _ => false
+  }
+  
+  
+  //restricts Expression by replacing variable with id i by value (should be Top or Bottom) and simplifying
   def restrictExpression(e: Expression, i: BigInt, value: Expression): Expression = {
+  	require(isConstant(value))
     e match {
       case Top        => Top
       case Bottom       => Bottom
@@ -117,29 +122,29 @@ object sharedOBDDs {
     }
   }
 
-  def build(b: BDD, e: Expression): (BDD, BigInt) = {
+  def build(b: BDD, e: Expression): RootedBDD = {
     val n = maxVarLabel(e, 0) //assumption: variables are labelled with integers starting from 1
-    def buildRec(b1: BDD, e: Expression, i: BigInt): (BDD, BigInt) = {
+    def buildRec(b1: BDD, e: Expression, i: BigInt): RootedBDD = {
 //       require(e == restrictExpression(
       if (i > n) {
         e match { // no more variables -> only constants possible
-          case Top  => (b1, 1)
-          case Bottom => (b1, 0)
+          case Top  => RootedBDD(b1, 1)
+          case Bottom => RootedBDD(b1, 0)
         }
       } else {
-        val (bdd1, j1) = buildRec(b1, restrictExpression(e, i, Bottom), i + 1)
-        val (bdd2, j2) = buildRec(bdd1, restrictExpression(e, i, Top), i + 1)
-        testAndInsert(bdd2, i, j1, j2)
+        val RootedBDD(bdd1, r1) = buildRec(b1, restrictExpression(e, i, Bottom), i + 1)
+        val RootedBDD(bdd2, r2) = buildRec(bdd1, restrictExpression(e, i, Top), i + 1)
+        testAndInsert(bdd2, Node(i, r1, r2))
       }
     }
 
     buildRec(b, e, 1)
   }
 
-  def maxVarLabel(e: Expression, max: BigInt): BigInt = {
+  def maxVarLabel(e: Expression, max: BigInt): BigInt = {//TODO refactor
     e match {
-      case Top         => max
-      case Bottom        => max
+      case Top          => max
+      case Bottom       => max
       case Variable(i)  => if (i > max) i else max
       case Negation(e1) => maxVarLabel(e1, max)
       case Conjunction(e1, e2) => {
@@ -150,53 +155,49 @@ object sharedOBDDs {
         val max1 = maxVarLabel(e1, max)
         maxVarLabel(e2, max1)
       }
-
     }
   }
   
-  
   def containsAllChildren(b: BDD, r: BigInt): Boolean = {
     b.T.contains(r) &&
-    containsAllChildren(b, low(b,r)) &&
-    containsAllChildren(b, high(b,r)) 
+    containsAllChildren(b, getNode(b,r).low) &&
+    containsAllChildren(b, getNode(b,r).high) 
   }
   
   def allChildrenInSet(b: BDD, r: BigInt, s: List[BigInt]): Boolean = {
     s.contains(r) &&
-    allChildrenInSet(b, low(b,r), s) &&
-    allChildrenInSet(b, high(b,r), s) 
+    allChildrenInSet(b, getNode(b,r).low, s) &&
+    allChildrenInSet(b, getNode(b,r).high, s) 
   }
-
-  
   
   //no dynamic programming used here in order to stay purely functional
-  def apply(b: BDD, op: (Boolean, Boolean) => Boolean, r1: BigInt, r2: BigInt): (BDD, BigInt) = {
+  def apply(b: BDD, op: (Boolean, Boolean) => Boolean, r1: BigInt, r2: BigInt): RootedBDD = {
     require(containsAllChildren(b, r1) && containsAllChildren(b, r2) && wellFormed(b.nodes, b.T))
   
     if ((r1 == 0 || r1 == 1) && (r2 == 0 || r2 == 1)) //two Terminals
-      if (op(r1 == 1, r2 == 1)) (b, BigInt(1)) 
-      else (b, BigInt(0))
-    else if (variable(b, r1) == variable(b, r2)) {
-      val loApp = apply(b, op, low(b, r1), low(b, r2))
+      if (op(r1 == 1, r2 == 1)) RootedBDD(b, BigInt(1)) 
+      else RootedBDD(b, BigInt(0))
+    else if (getNode(b, r1).variable == getNode(b, r2).variable) {
+      val RootedBDD(bLow, rLow) = apply(b, op, getNode(b, r1).low, getNode(b, r2).low)
       // b.nodes subset of loApp.nodes
       
       
-      val hiApp = apply(loApp._1, op, high(b, r1), high(b, r2))
-      testAndInsert(hiApp._1, variable(b, r1), loApp._2, hiApp._2)
+      val RootedBDD(bHigh, rHigh) = apply(bLow, op, getNode(b, r1).high, getNode(b, r2).high)
+      testAndInsert(bHigh, Node(getNode(b, r1).variable, rLow, rHigh))
     } 
 //     else (b,r1)
-    else if (variable(b, r1) < variable(b, r2)) {
-      val loApp = apply(b, op, low(b, r1), r2)
-      val hiApp = apply(loApp._1, op, high(b, r1), r2)
-      testAndInsert(hiApp._1, variable(b, r1), loApp._2, hiApp._2)
+    else if (getNode(b, r1).variable < getNode(b, r2).variable) {
+      val RootedBDD(bLow, rLow) = apply(b, op, getNode(b, r1).low, r2)
+      val RootedBDD(bHigh, rHigh) = apply(bLow, op, getNode(b, r1).high, r2)
+      testAndInsert(bHigh, Node(getNode(b, r1).variable, rLow, rHigh))
     } else {
-      val loApp = apply(b, op, r1, low(b, r2))
-      val hiApp = apply(loApp._1, op, r1, high(b, r2))
-      testAndInsert(hiApp._1, variable(b, r2), loApp._2, hiApp._2)
+      val RootedBDD(bLow, rLow) = apply(b, op, r1, getNode(b, r2).low)
+      val RootedBDD(bHigh, rHigh) = apply(bLow, op, r1, getNode(b, r2).high)
+      testAndInsert(bHigh, Node(getNode(b, r2).variable, rLow, rHigh))
     }
 
-  } ensuring ((res: (BDD, BigInt)) => {
-    val (bdd,r) = res
+  } ensuring ((res: RootedBDD) => {
+    val RootedBDD(bdd,r) = res
     b.nodes.content.subsetOf(bdd.nodes.content) &&
     wellFormed(bdd.nodes, bdd.T)
   })
@@ -209,72 +210,72 @@ object sharedOBDDs {
 
   def minus(b: BDD, r1: BigInt, r2: BigInt) = {
     val bNew = complement(b, r2)
-    intersect(bNew._1, r1, bNew._2)
+    intersect(bNew.b, r1, bNew.root)
   }
 
-  def complement(b: BDD, root: BigInt): (BDD, BigInt) = apply(b, _ != _, root, 1)
+  def complement(b: BDD, root: BigInt) = apply(b, _ != _, root, 1)
 
   def equivalent(r1: BigInt, r2: BigInt) = r1 == r2
 
 
-  //TODO use restrict algo instead??
+  //use restrict algo instead?
   //precond: all variables in map 
-  def evaluate(b: BDD, root: BigInt, interpretaion: Map[BigInt, Boolean]): Boolean = {
-    if (root == 1)
+  def evaluate(bdd: RootedBDD, interpretaion: Map[BigInt, Boolean]): Boolean = {
+    if (bdd.root == 1)
       true
-    else if (root == 0)
+    else if (bdd.root == 0)
       false
     else {
-      if (interpretaion(variable(b, root)))
-        evaluate(b, high(b, root), interpretaion)
+      if (interpretaion(getNode(bdd.b, bdd.root).variable))
+        evaluate(RootedBDD(bdd.b, getNode(bdd.b, bdd.root).high), interpretaion)
       else
-        evaluate(b, low(b, root), interpretaion)
+        evaluate(RootedBDD(bdd.b, getNode(bdd.b, bdd.root).low), interpretaion)
     }
   }
 
-  def restrict(b: BDD, root: BigInt, eliminatedVar: BigInt, value: Boolean): (BDD, BigInt) = {
-    if (variable(b, root) > eliminatedVar)
-      (b, root)
-    else if (variable(b, root) < eliminatedVar) {
-      val lo = restrict(b, low(b, root), eliminatedVar, value)
-      val hi = restrict(lo._1, high(b, root), eliminatedVar, value)
-      testAndInsert(b, variable(b, root), lo._2, hi._2)
+  def restrict(bdd: RootedBDD, eliminatedVar: BigInt, value: Boolean): RootedBDD = {
+    if (getNode(bdd.b, bdd.root).variable > eliminatedVar)
+		bdd
+    else if (getNode(bdd.b, bdd.root).variable < eliminatedVar) {
+      val RootedBDD(bLow, rLow) = restrict(RootedBDD(bdd.b, getNode(bdd.b, bdd.root).low), eliminatedVar, value)
+      val RootedBDD(bHigh, rHigh) = restrict(RootedBDD(bLow, getNode(bdd.b, bdd.root).high), eliminatedVar, value)
+      testAndInsert(bHigh, Node(getNode(bdd.b, bdd.root).variable, rLow, rHigh))
     } else if (value) //variable(b, root) == eliminatedVar
-      restrict(b, high(b, root), eliminatedVar, value)
+      restrict(RootedBDD(bdd.b, getNode(bdd.b, bdd.root).high), eliminatedVar, value)
     else
-      restrict(b, low(b, root), eliminatedVar, value)
+      restrict(RootedBDD(bdd.b, getNode(bdd.b, bdd.root).low), eliminatedVar, value)
   }
 
-  def exists(b: BDD, root: BigInt, consideredVar: BigInt): (BDD, BigInt) = {
-    val resToZero = restrict(b, root, consideredVar, false)
-    val resToOne = restrict(resToZero._1, root, consideredVar, true)
-    apply(resToOne._1, _ || _, resToZero._2, resToOne._2)
+  def exists(bdd: RootedBDD, consideredVar: BigInt): RootedBDD = {
+    val RootedBDD(b0, r0) = restrict(bdd, consideredVar, false)
+    val RootedBDD(b1, r1)  = restrict(RootedBDD(b0, bdd.root), consideredVar, true)
+    apply(b1, _ || _, r0, r1)
   }
 
   //precond: lists nonempty + same size
-  def rename(b: BDD, root: BigInt, oldNames: List[BigInt], newNames: List[BigInt]): (BDD, BigInt) = {
-    if (root == 0 || root == 1)
-      (b, root)
+  def rename(bdd: RootedBDD, oldNames: List[BigInt], newNames: List[BigInt]): RootedBDD = {
+    if (bdd.root == 0 || bdd.root == 1)
+      bdd
     else {
-      val lo = rename(b, low(b, root), oldNames, newNames)
-      val hi = rename(lo._1, high(b, root), oldNames, newNames)
+      val RootedBDD(bLow, rLow) = rename(RootedBDD(bdd.b, getNode(bdd.b, bdd.root).low), oldNames, newNames)
+      val RootedBDD(bHigh, rHigh) = rename(RootedBDD(bLow, getNode(bdd.b, bdd.root).high), oldNames, newNames)
       
-      val name = if (oldNames.contains(variable(b, root))) newNames(oldNames.indexOf(variable(b, root)))
-      else variable(b, root)
+      val name = if (oldNames.contains(getNode(bdd.b, bdd.root).variable)) newNames(oldNames.indexOf(getNode(bdd.b, bdd.root).variable))
+      else getNode(bdd.b, bdd.root).variable
 
-      testAndInsert(hi._1, name, lo._2, hi._2)
+      testAndInsert(bHigh, Node(name, rLow, rHigh))
     }
   }
 
   //TODO implement
-  def preE(b: BDD, root: BigInt, transitions: BDD, rootTrans: BigInt): (BDD, BigInt) = (b, root)
+  def preE(statesSubset: RootedBDD, transitions: RootedBDD): RootedBDD = statesSubset
     //val bPrimed = rename(b, )//TODO this is inefficient -> unprimed and primed variables should be interleaved
   
 
-  def preA(b: BDD, root: BigInt, rootX: BigInt, transitions: BDD, transRoot: BigInt): (BDD, BigInt) = {
-    val complementSet = minus(b, root, rootX)
-    val preExists = preE(complementSet._1, complementSet._2, transitions, transRoot)
-    minus(preExists._1, root, preExists._2)
+  def preA(states: RootedBDD, rootSubset: BigInt, transitions: RootedBDD): RootedBDD = {
+    val complement = minus(states.b, states.root, rootSubset)
+    val RootedBDD(bPreE, rPreE) = preE(complement, transitions)
+    minus(bPreE, states.root, rPreE)
   }
   
   def extend(s: Set[List[Boolean]], i: BigInt): Set[List[Boolean]] = {
@@ -284,16 +285,22 @@ object sharedOBDDs {
       extend(setToList(s).map(l => false::l).content ++ setToList(s).map(l => true::l).content, i - 1)
   }
   
-  def contentLists(b: BDD, root: BigInt) : Set[List[Boolean]] = {
-    if(root == 0)
+  def contentLists(bdd: RootedBDD) : Set[List[Boolean]] = {
+    if(bdd.root == 0)
       Set()
-    else if(root == 1)
+    else if(bdd.root == 1)
       Set(List[Boolean]())
     else {
-      val lo = if(variable(b, root) - variable(b, low(b, root)) == 1) contentLists(b, low(b, root))
-                else extend(contentLists(b, low(b, root)), variable(b, root) - variable(b, low(b, root)) - 1)
-      val hi = if(variable(b, root) - variable(b, low(b, root)) == 1) contentLists(b, low(b, root))
-                  else extend(contentLists(b, low(b, root)), variable(b, root) - variable(b, low(b, root)) - 1)
+      val lo = if(getNode(bdd.b, bdd.root).variable - getNode(bdd.b, getNode(bdd.b, bdd.root).low).variable == 1)
+                 contentLists(RootedBDD(bdd.b, getNode(bdd.b, bdd.root).low))
+               else
+                 extend(contentLists(RootedBDD(bdd.b, getNode(bdd.b, bdd.root).low)),
+                   getNode(bdd.b, bdd.root).variable - getNode(bdd.b, getNode(bdd.b, bdd.root).low).variable - 1)
+      val hi = if(getNode(bdd.b, bdd.root).variable - getNode(bdd.b, getNode(bdd.b, bdd.root).high).variable == 1)
+                 contentLists(RootedBDD(bdd.b, getNode(bdd.b, bdd.root).high))
+               else
+                 extend(contentLists(RootedBDD(bdd.b, getNode(bdd.b, bdd.root).high)),
+                   getNode(bdd.b, bdd.root).variable - getNode(bdd.b, getNode(bdd.b, bdd.root).high).variable - 1)
       setToList(lo).map(l => false::l).content ++ setToList(hi).map(l => true::l).content
     }
   }
@@ -304,6 +311,6 @@ object sharedOBDDs {
   
   def correctUnion(b: BDD, root1: BigInt, root2: BigInt) : Boolean = {
   	val bddUnion = union(b, root1, root2)
-  	(b.T contains root1) && (contentLists(bddUnion._1, bddUnion._2) == contentLists(b, root1) ++ contentLists(b, root2))
+  	(contentLists(bddUnion) == contentLists(RootedBDD(b, root1)) ++ contentLists(RootedBDD(b, root2)))
   } holds
 }
