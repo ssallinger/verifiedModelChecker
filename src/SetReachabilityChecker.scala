4bff861829ba9e7e3e9ba43ac2c7441c1a682eae
Sarah Sallinger <sallingersa@stud.sbg.ac.at>
2016-08-31 12:40:00 +0200

    Add proof ideas

--- src/SetReachabilityChecker.scala
+++ src/SetReachabilityChecker.scala
@@ -17,17 +17,39 @@ object SetReachabilityChecker {
   case class Trace(states: List[State]) extends Result
 
   def kReachabilityCheck(currentTrace: List[State], s: System, target: State, k: BigInt) : Result = {
-    require(!currentTrace.isEmpty && isTrace(s, currentTrace))
+    require(!currentTrace.isEmpty &&
+            isTrace(s, currentTrace) &&
+            //containsForall(successors(s.transitions, currentTrace.head)) &&
+            allSuccessorsTransition(s, currentTrace.head, successors(s.transitions, currentTrace.head)) &&
+            successors(s.transitions, currentTrace.head).forall(s1 => s.transitions.contains((currentTrace.head, s1)))
+    )
     
     if(currentTrace.head == target)
       Trace(currentTrace)
     else if(k == 0)
       Unknown
     else {
-      val succ = successors(s.transitions, currentTrace.head)
-      exploreSuccessors(succ, currentTrace, s, target, k - 1, false)
+      exploreSuccessors(successors(s.transitions, currentTrace.head), currentTrace, s, target, k - 1, false)
     }
   }
+  
+  //@ignore
+  def containsForall(l: List[State]) : Boolean = {
+    l match {
+      case Nil() => l.forall(x => l.contains(x))
+      case x :: xs => l.contains(x) && containsForall(xs) && xs.forall(y => l.contains(y)) && l.forall(y => l.contains(y))
+    }
+    //l.forall(x => l.contains(x))
+  } holds
+  
+  //TODO
+  def listContainsTail[X](l :List[X]) : Boolean = {
+    l match {
+      case Nil() => trivial
+      case x :: xs => true
+    }
+    
+  } holds
 
   def exploreSuccessors(succ: List[State], currentTrace: List[State], s: System, target: State, k: BigInt, unknown: Boolean) : Result = {
     require(isTrace(s, currentTrace) && (currentTrace.isEmpty || succ.forall(s1 => s.transitions.contains((currentTrace.head, s1)))))
@@ -43,7 +65,7 @@ object SetReachabilityChecker {
       }
     }
   }
-
+  
   def successors(tr: List[(State, State)], s: State) : List[State] = {
    tr match {
      case Nil() => List()
@@ -59,6 +81,24 @@ object SetReachabilityChecker {
       case Nil() => tr.contains((s, st))
       case x :: xs => ((successors(xs, s).contains(st) && successorsTransition(xs, s, st)) || (x._2 == st && x._1 == s)) &&
                       tr.contains((s, st))
+    }
+  } holds
+  
+  def allSuccessorsTransition(sys: System, s: State, res: List[State]) : Boolean = {
+    require(res.forall(st => successors(sys.transitions, s).contains(st)))
+    res match {
+      case Nil() => res.forall(st => sys.transitions.contains((s, st)))
+      case x :: xs => allSuccessorsTransition(sys, s, xs) &&
+                      successors(sys.transitions, s).contains(x) &&
+                      successorsTransition(sys.transitions, s, x) &&
+                      res.forall(st => sys.transitions.contains((s, st)))
+    }
+  } holds
+  
+  def test(res: List[State]) : Boolean = {
+    res match {
+      case Nil() => trivial
+      case x :: xs => test(xs) && res.forall(st => true)
     }
   } holds
   
@@ -88,5 +128,4 @@ object SetReachabilityChecker {
     }
   } holds
 
-  
 }
