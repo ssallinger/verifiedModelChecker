6d9e9c7e451df8a3fc25d19c0a39cc23c4e2d254
Sarah Sallinger <sallingersa@stud.sbg.ac.at>
2016-09-01 11:45:17 +0200

    prove correctTrace

--- src/SetReachabilityChecker.scala
+++ src/SetReachabilityChecker.scala
@@ -15,56 +15,9 @@ object SetReachabilityChecker {
   case object Unknown extends Result
   case object Unreachable extends Result
   case class Trace(states: List[State]) extends Result
-
-  def kReachabilityCheck(currentTrace: List[State], s: System, target: State, k: BigInt) : Result = {
-    require(!currentTrace.isEmpty &&
-            isTrace(s, currentTrace) //&&
-            //containsForall(successors(s.transitions, currentTrace.head)) &&
-            //allSuccessorsTransition(s, currentTrace.head, successors(s.transitions, currentTrace.head)) &&
-            //successors(s.transitions, currentTrace.head).forall(s1 => s.transitions.contains((currentTrace.head, s1)))
-    )
-    
-    if(currentTrace.head == target)
-      Trace(currentTrace)
-    else if(k == 0)
-      Unknown
-    else {
-      exploreSuccessors(successors(s.transitions, currentTrace.head), currentTrace, s, target, k - 1, false)
-    }
-  }
   
-  //@ignore
-  /*def containsForall(l: List[State]) : Boolean = {
-    l match {
-      case Nil() => l.forall(x => l.contains(x))
-      case x :: xs => l.contains(x) && containsForall(xs) && xs.forall(y => l.contains(y)) && l.forall(y => l.contains(y))
-    }
-    //l.forall(x => l.contains(x))
-  } holds
   
-  //TODO
-  def listContainsTail[X](l :List[X]) : Boolean = {
-    l match {
-      case Nil() => trivial
-      case x :: xs => true
-    }
-    
-  } holds*/
-
-  def exploreSuccessors(succ: List[State], currentTrace: List[State], s: System, target: State, k: BigInt, unknown: Boolean) : Result = {
-    require(isTrace(s, currentTrace) && (currentTrace.isEmpty || succ.forall(s1 => s.transitions.contains((currentTrace.head, s1)))))
-    succ match {
-      case Nil() => if(unknown) Unknown else Unreachable
-         case x :: xs => {
-         val res = kReachabilityCheck(x :: currentTrace, s, target, k)
-         res match {
-           case Trace(t) => res //target found
-           case Unknown => exploreSuccessors(xs, currentTrace, s, target, k, true)
-           case Unreachable => exploreSuccessors(xs, currentTrace, s, target, k, unknown)
-         }
-      }
-    }
-  }
+  
   
   def successors(tr: List[(State, State)], s: State) : List[State] = {
    tr match {
@@ -75,6 +28,7 @@ object SetReachabilityChecker {
    }
   }
   
+  
   def successorsTransition(tr: List[(State, State)], s: State, st: State) : Boolean = {
     require (successors(tr, s).contains(st)) 
     tr match {
@@ -84,49 +38,124 @@ object SetReachabilityChecker {
     }
   } holds
   
-  def allSuccessorsTransition(sys: System, s: State, res: List[State]) : Boolean = {
-    require(res.forall(st => successors(sys.transitions, s).contains(st)))
-    res match {
-      case Nil() => res.forall(st => sys.transitions.contains((s, st)))
-      case x :: xs => allSuccessorsTransition(sys, s, xs) &&
-                      successors(sys.transitions, s).contains(x) &&
-                      successorsTransition(sys.transitions, s, x) &&
-                      res.forall(st => sys.transitions.contains((s, st)))
-    }
-  } holds
   
-  /*def test(res: List[State]) : Boolean = {
-    res match {
-      case Nil() => trivial
-      case x :: xs => test(xs) && res.forall(st => true)
-    }
-  } holds*/
-  
-  def isTrace(s: System, t: List[State]) : Boolean = {
-    t match {
-      case s1 :: s2 :: ts => 
-        s.transitions.contains((s2, s1)) &&
-        isTrace(s, s2 :: ts)
-      case _ => true
+  def canTransition(tr: List[(State,State)], x: State, l: List[State]): Boolean = {
+    l match {
+      case Nil() => true
+      case Cons(y,ys) => tr.contains((x,y)) && canTransition(tr, x, ys)
     }
   }
   
-  //TODO prove
-  @ignore
-  def correctTrace(s: System, initial: List[State], target: State, k: BigInt) : Boolean = {
-    val res = exploreSuccessors(initial, List(), s, target, k, false)
-    res match {
-      case Trace(t) =>
-        //initial.contains(t.last) &&
-        //t.head == target && 
-        isTrace(s, t)
-        /*t match {
-          case s1 :: s2 :: ts => 
-            s.transitions.contains((s2, s1))
-          case _ => true
-        }*/
-      case _ => true
+  
+  def canTransitionSuccHelp(tr: List[(State,State)], s: State, succ: List[State]): Boolean = {
+    //val succ = successors(tr,s)
+    
+    require(subset(succ, successors(tr,s)))
+    succ match {
+      case Nil() => true
+      case Cons(x, xs) => successorsTransition(tr, s, x) && tr.contains((s,x)) && canTransitionSuccHelp(tr, s, xs) && canTransition(tr, s, succ)
     }
+    //canTransition(tr, s, succ)
   } holds
+  
+  def canTransitionSucc(tr: List[(State,State)], s: State): Boolean = {
+    val succ = successors(tr,s)
+    
+    subsetReflexive(succ) &&
+    canTransitionSuccHelp(tr, s, succ) &&
+    canTransition(tr, s, succ)
+    
+  } holds
+  
+  def subset[X](l1: List[X], l2: List[X]) : Boolean = {
+    l1 match {
+      case Nil() => true
+      case Cons(x, xs) => l2.contains(x) && subset(xs,  l2)
+    }
+  }
+  
+  def subsetReflexive[X](l1: List[X]) : Boolean = {
+    l1 match {
+      case Nil() => true
+      case Cons(x, xs) => l1.contains(x) && subsetReflexive(xs) && subsetExtend(xs, xs, x) && subset(xs, l1) && subset(l1, l1)
+      //wissen: 
+    }
+    //subset(l1, l1)
+  } holds
+  
+  @induct
+  def subsetExtend[X](l1: List[X], l2: List[X], x: X) : Boolean = {
+    require(subset(l1, l2))
+    
+    subset(l1, x :: l2)
+  } holds
+
+   def kReachabilityCheck(currentTrace: List[State], s: System, target: State, k: BigInt, initial: State) : Result = {
+     require(!currentTrace.isEmpty &&
+             isTrace(s, currentTrace) &&
+             canTransitionSucc(s.transitions, currentTrace.head) &&
+             currentTrace.last == initial
+             //&&
+             //containsForall(successors(s.transitions, currentTrace.head)) &&
+             //allSuccessorsTransition(s, currentTrace.head, successors(s.transitions, currentTrace.head)) &&
+             //successors(s.transitions, currentTrace.head).forall(s1 => s.transitions.contains((currentTrace.head, s1)))
+     )
+     
+     if(currentTrace.head == target)
+       Trace(currentTrace)
+     else if(k == 0)
+       Unknown
+     else {
+       // canTransition(s.transitions, currentTrace.head, successors(s.transitions, currentTrace.head))
+       exploreSuccessors(successors(s.transitions, currentTrace.head), currentTrace, s, target, k - 1, false, initial)
+     }
+   } ensuring(res => res match {
+                        case Trace(t) => isTrace(s, t) && !t.isEmpty && t.last == initial && t.head == target
+                        case _ => true
+                     }
+   )
+ 
+   def exploreSuccessors(succ: List[State], currentTrace: List[State], s: System, target: State, k: BigInt, unknown: Boolean, initial: State) : Result = {
+     require(isTrace(s, currentTrace) && !currentTrace.isEmpty && canTransition(s.transitions, currentTrace.head, succ) && currentTrace.last == initial)
+     
+     succ match {
+       case Nil() => if(unknown) Unknown else Unreachable
+       case Cons(x,xs) if (!currentTrace.contains(x))=>{
+       
+          val res = kReachabilityCheck(x :: currentTrace, s, target, k, initial)
+          res match {
+            case Trace(t) => res //target found
+            case Unknown => exploreSuccessors(xs, currentTrace, s, target, k, true, initial)
+            case Unreachable => exploreSuccessors(xs, currentTrace, s, target, k, unknown, initial)
+          }
+       }
+       case Cons(_,xs) => exploreSuccessors(xs, currentTrace, s, target, k, unknown, initial)
+     }
+   } ensuring(res => res match {
+                        case Trace(t) => isTrace(s, t) && !t.isEmpty && t.last == initial && t.head == target
+                        case _ => true
+                     }
+   )
+  
+  
+   def isTrace(s: System, t: List[State]) : Boolean = {
+     t match {
+       case s1 :: s2 :: ts => 
+         s.transitions.contains((s2, s1)) &&
+         isTrace(s, s2 :: ts)
+       case _ => true
+     }
+   }
+   
+   def correctTrace(s: System, initial: State, target: State, k: BigInt) : Boolean = {
+     val res = kReachabilityCheck(List(initial), s, target, k, initial)
+     res match {
+       case Trace(t) =>
+         initial == t.last &&
+         t.head == target && 
+         isTrace(s, t)
+       case _ => true
+     }
+   } holds
 
 }
